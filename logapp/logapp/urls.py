e95ac99ebffb6659f41fcf38b761236151c27eb7
UncSald <iikka.alingue@gmail.com>
2026-04-01 15:58:48 +0300

    added a login page, reordered the urls

--- logapp/logapp/urls.py
+++ logapp/logapp/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', include('logins.urls')),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='login.html')),
+    path('logout/', LogoutView.as_view(next_page='/')),
+    path('', include('logins.urls')),   
 ]
