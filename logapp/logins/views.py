a7fed1d17a96372461c0c58927a3a268ff8b6500
UncSald <iikka.alingue@gmail.com>
2026-04-13 16:43:40 +0300

    Added path to logpage.html

--- logapp/logins/views.py
+++ logapp/logins/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Log, WorkingTime
+from django.contrib.auth.models import User
+
 
 @login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    users = Log.objects.filter(user=request.user)
+    return render(request, 'logpage.html', {'users': users})
+
