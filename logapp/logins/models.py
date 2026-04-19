90eb0d4ea0c8acc0f034243653b2fe6a6c22294a
UncSald <iikka.alingue@gmail.com>
2026-04-19 22:26:47 +0300

    Added model for working time which can be connected to another model

--- logapp/logins/models.py
+++ logapp/logins/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Log(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     time = models.DateTimeField('log timestamp')
+    login = models.BooleanField(default=True)
     note = models.ForeignKey('Note', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
@@ -18,3 +19,7 @@ class Note(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class WorkingTime(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_worked = int
