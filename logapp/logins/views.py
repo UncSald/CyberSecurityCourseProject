71cd399ad2e24bbde5eb69c59d8a8a8d92b15703
UncSald <iikka.alingue@gmail.com>
2026-04-19 22:27:27 +0300

    Added creation of new logs by introducing a csrf vulnerability

--- logapp/logins/views.py
+++ logapp/logins/views.py
@@ -6,8 +6,31 @@ from .models import Log, WorkingTime
 from django.contrib.auth.models import User
 
 
-@login_required
-def index(request):
+def front(request):
+    if request.user.is_anonymous:
+        print('lol', request.user)
+        return redirect('login/')
     user_logs = Log.objects.filter(user=request.user)
     return render(request, 'logpage.html', {'logs': user_logs})
 
+def create_log(request):
+    if request.method=="GET":
+        request.session['timeoflog'] = request.GET.get('timeoflog')
+        request.session['login'] = request.GET.get('login')
+
+    return render(request, 'confirm.html')
+
+def confirm_creation(request):
+    typeoflog = False
+    time = request.session['timeoflog']
+    to = request.session['login']
+    if to == True:
+        typeoflog = True
+    new_log = Log(user=request.user, time=time, login=typeoflog)
+    new_log.save()
+    return redirect('/')
+
+@login_required
+def most_hours(request):
+
+    return render(request, 'toplist.html')
