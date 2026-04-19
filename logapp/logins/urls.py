4a9fe6a6f24684ffde9c16c690ef7ea6dd208677
UncSald <iikka.alingue@gmail.com>
2026-04-19 22:27:49 +0300

    Added urls for creation and confirmation

--- logapp/logins/urls.py
+++ logapp/logins/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.front, name='front'),
+    path('create_log/', views.create_log, name='create_log'),
+    path('confirm_creation/', views.confirm_creation, name='confirm'),
+    path('toplist/', views.most_hours, name='toplist')
 ]
