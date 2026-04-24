5193dab667c7d1eb349416ec8948213e7c64c156
UncSald <iikka.alingue@gmail.com>
2026-04-24 12:37:01 +0100

    Changed create log path

--- logapp/logins/urls.py
+++ logapp/logins/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.front, name='front'),
     path('user/<str:name>/', views.user_view, name='user'),
-    path('create_log/', views.create_log, name='create_log'),
+    path('user/<str:name>/create_log/', views.create_log, name='create_log'),
     path('confirm_creation/', views.confirm_creation, name='confirm'),
     path('toplist/', views.most_hours, name='toplist')
 ]
