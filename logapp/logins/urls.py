be79b45afa8e976a36084a82dfab655acc9b7d39
UncSald <iikka.alingue@gmail.com>
2026-04-24 11:26:19 +0100

    Changed the url for a logged in user

--- logapp/logins/urls.py
+++ logapp/logins/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.front, name='front'),
+    path('user/<str:name>/', views.user_view, name='user'),
     path('create_log/', views.create_log, name='create_log'),
     path('confirm_creation/', views.confirm_creation, name='confirm'),
     path('toplist/', views.most_hours, name='toplist')
