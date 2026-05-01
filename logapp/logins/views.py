2f4970ff87a7c60bf2094cf435853edbb94a67c8
UncSald <iikka.alingue@gmail.com>
2026-05-01 16:01:11 +0100

    Note is added only if it has some content

--- logapp/logins/views.py
+++ logapp/logins/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Log, WorkingTime
+from .models import Log, WorkingTime, Note
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_protect
 
 
 def front(request):
@@ -14,16 +14,18 @@ def front(request):
 @login_required
 def user_view(request, name):
     try:
+        if name != request.user.username: raise Exception
         users = User.objects.get(username=name)
-        if users:
-            user_logs = Log.objects.filter(user=users)
-            return render(request, 'logpage.html', {'user':name,'logs': user_logs})
+        user_logs = Log.objects.filter(user=users)
+        return render(request, 'logpage.html', {'user':name,'logs': user_logs})
     except:
         return redirect('/')
 
 @login_required
 def create_log(request,name):
     if request.method=="GET":
+    # Fix csrf:
+    # if request.method=="POST":
         request.session['timeoflog'] = request.GET.get('timeoflog')
         request.session['login'] = request.GET.get('login')
         request.session['note'] = request.GET.get('note')
@@ -38,9 +40,13 @@ def confirm_creation(request):
         time = request.session['timeoflog']
         log = request.session['login']
         note = request.session['note']
-        new_note = Note(user=user, note_content=note)
-        new_note.save()
-        new_log = Log(user=user, time=time, login=log, note=new_note if note != '' else None)
+        if note !='':
+            new_note = Note(user=user, note_content=note)
+            new_note.save()
+        new_log = Log(user=user,
+                      time=time,
+                      login=log,
+                      note=new_note if 'new_note' in locals() else None)
         new_log.save()
     return redirect('/')
 
